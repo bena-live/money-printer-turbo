65d51aae49e3c13d9693c31e6b5b3bc16e01734f
758245131@qq.com <758245131@qq.com>
2025-06-18 18:04:46 +0800

    update: Reduce generation time

--- app/services/video.py
+++ app/services/video.py
@@ -122,7 +122,11 @@ def random_to_begin_video(
     max_clip_duration: int = 5,
 ):
     begin_videos_dir = utils.storage_dir("begin_videos", create=True)
-    begin_list = [ "1.mp4", "2.mp4", "3.mp4", "4.mp4", "5.mp4" ]
+    begin_list = [
+        "1.mp4", "2.mp4", "3.mp4", "4.mp4", "5.mp4",
+        "6.mp4", "7.mp4", "8.mp4", "9.mp4", "10.mp4",
+        "11.mp4", "12.mp4"
+    ]
     random_path = os.path.join(begin_videos_dir, random.choice(begin_list))
     logger.info(f"random begin video: {random_path}")
 
